41a62ea168f23df911de1414e24f3a11fa846031
José Rodrigues <jyrodrigues@gmail.com>
2018-09-11 14:31:26 -0300

    Replace App.tsx styles from css file to styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,65 @@
 import * as React from 'react';
-import './App.css';
-
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <AppWrapper>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
+          <AppTitle>Welcome to React</AppTitle>
+        </AppHeader>
+        <AppIntro>
+          To get started, edit <CodeWrapper>src/App.tsx</CodeWrapper> and save to reload.
+        </AppIntro>
+      </AppWrapper>
     );
   }
 }
+
+
+// STYLED COMPONENTS
+
+const AppWrapper = styled.div`
+  text-align: center;
+`
+
+const AppHeader = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg)
+  }
+  to {
+    transform: rotate(360deg)
+  }
+`
+
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 20s linear;
+  height: 80px;
+  &:hover {
+    animation: ${rotate360} infinite 1.5s linear;
+  }
+`
+
+const AppTitle = styled.h1`
+  font-size: 1.5em
+`
+
+const CodeWrapper = styled.code`
+  font-size: 1.3rem;
+`
+
+const AppIntro = styled.p`
+  color: ${props => props.theme.dark};
+  font-size: large;
+`
 
 export default App;
